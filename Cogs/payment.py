8662f2449c8856830e837708cf9af0fbbc60b60d
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-05-11 01:50:31 +0530

    update payment.py

--- Cogs/payment.py
+++ Cogs/payment.py
@@ -10,8 +10,8 @@ class Payment(commands.Cog):
     @commands.command()
     @commands.cooldown(1, 10, commands.BucketType.user)
     async def buy(self, ctx, points: int = None):
-        if ctx.guild: await ctx.send(ctx.author.mention + "**, Let's continue in DM!**")
         if not points: return await ctx.send(ctx.author.mention + ", You didn't enter any points amount.")
+        if ctx.guild: await ctx.send(ctx.author.mention + "**, Let's continue in DM!**")
         embed = discord.Embed(title = "Payment Instructions !",
             description = "Please send exactly **₹{}** to the following QR! After payment send your Order ID or Transaction ID here within 5 minutes. Your points will be given within 10 minutes.".format(points),
             color = discord.Colour.random())
@@ -21,11 +21,11 @@ class Payment(commands.Cog):
         try:
             message = await self.client.wait_for("message", timeout = 300.0)
         except:
-            return await ctx.send(ctx.author.mention + ", You failed to send your order ID within time. Don't worry if already paid the amount then start this session again and send your ID.")
+            return await ctx.author.send(ctx.author.mention + ", You failed to send your order ID within time. Don't worry if already paid the amount then start this session again and send your ID.")
         try:
             id = int(message)
         except:
-            return await ctx.send(ctx.author.mention + ", Invalid Order ID!")
+            return await ctx.author.send(ctx.author.mention + ", Invalid Order ID!")
             
         channel = self.client.get_channel(973640257612431401)
         embed = discord.Embed(title = "Payment Information !",
