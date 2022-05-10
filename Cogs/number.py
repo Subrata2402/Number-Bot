c138c68e305868730a95a72b5c29db00b8c4a419
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-05-10 17:53:09 +0530

    update number.py

--- Cogs/number.py
+++ Cogs/number.py
@@ -1,1 +1,18 @@
+import discord, services
+from discord.ext import commands
+from NumberApi.number_api import NumberApi
+from database import db
 
+class Number(commands.Cog, NumberApi):
+    
+    def __init__(self, client):
+        super().__init__()
+        self.client = client
+
+    @commands.command()
+    async def get(self, service: str = None):
+        if not service: return await ctx.send("Please provide a service name to request a number.")
+        price = services.get(service.lower())
+        if not price:
+            return await ctx.send(f"Invalid service name. Please use `{ctx.prefix}price` to get a list of price of service.")
+        
