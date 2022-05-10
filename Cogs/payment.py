388aed2abc29acd52b28f268c4d5479cad0e5972
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-05-10 18:57:56 +0530

    update payment.py

--- Cogs/payment.py
+++ Cogs/payment.py
@@ -1,1 +1,13 @@
+import discord
+from discord.ext import commands
+from database import db
 
+class UserDetails(commands.Cog):
+    
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def buy(self, points: int = None):
+        if not points: return await ctx.send(ctx.author.mention + ", You didn't enter any points amount.")
+        
