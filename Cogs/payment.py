50e4e225d0b02f8043d05e4fc3035437aef18d9e
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-05-10 20:57:24 +0530

    update payment.py

--- Cogs/payment.py
+++ Cogs/payment.py
@@ -9,5 +9,7 @@ class UserDetails(commands.Cog):
 
     @commands.command()
     async def buy(self, points: int = None):
+        if ctx.guild: await ctx.send(ctx.author.mention + "**, Let's continue in DM!**")
         if not points: return await ctx.send(ctx.author.mention + ", You didn't enter any points amount.")
-        
+        embed = discord.Embed(title = "Payment Instructions !",
+            description = "Please send exactly "
