328d4c418d7dbb325bc665b8cdaf8cc4d8c1e58a
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-05-11 01:38:57 +0530

    update number.py

--- Cogs/number.py
+++ Cogs/number.py
@@ -15,7 +15,7 @@ class Number(commands.Cog, NumberApi):
     async def price(self, ctx):
         description = ""
         embed = discord.Embed(title = "Price list of Services !", color = discord.Colour.random())
-        for key, value in services.service_list:
+        for key, value in services.service_list.items():
             description += "• {} - ₹{}".format(key, value)
         embed.description = "```\n{}\n```".format(description)
         await ctx.send(embed = embed)
