4e1737b941b91eb4408c72404d76b5a8acc4303a
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-05-11 01:03:24 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-data = MongoClient('mongodb+srv://Subrata2001:Subrata2001@cluster0.ywnwn.mongodb.net/NumberBot?retryWrites=true&w=majority')
+data = MongoClient('mongodb+srv://Subrata3250:Subrata3250@cluster0.ywnwn.mongodb.net/NumberBot?retryWrites=true&w=majority')
 db = data.get_database("NumberBot")
-points = db.points
+user = db.points
