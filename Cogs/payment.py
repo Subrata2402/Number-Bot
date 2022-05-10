4e0f57ba8965da6fbe0fb3edbc00396cb8f1ef57
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-05-10 21:02:48 +0530

    update payment.py

--- Cogs/payment.py
+++ Cogs/payment.py
@@ -12,4 +12,7 @@ class UserDetails(commands.Cog):
         if ctx.guild: await ctx.send(ctx.author.mention + "**, Let's continue in DM!**")
         if not points: return await ctx.send(ctx.author.mention + ", You didn't enter any points amount.")
         embed = discord.Embed(title = "Payment Instructions !",
-            description = "Please send exactly "
+            description = "Please send exactly **₹{}** to the following QR! After payment send your Order ID or Transaction ID here within 5 minutes. Your points will be given within 10 minutes.".format(points),
+            color = discord.Colour.random())
+        embed.set_image(url = url)
+        embed.set_footer(text = "Payment Created by : {}".format(ctx.author))
