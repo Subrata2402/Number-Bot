baa981f1574fb59b71f44f4ea11969524e95555f
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-05-10 17:22:59 +0530

    update number_api.py

--- NumberApi/number_api.py
+++ NumberApi/number_api.py
@@ -3,6 +3,9 @@ import aiohttp, asyncio
 class BaseHQApi:
     def __init__(self, access_code: str = None):
         self.access_code = access_code
-        
+        self.host = "https://autobuyotp.com/sms/sms2.php"
     
- 
+    async def fetch(self, method = "GET", function = "", headers = None, data = None):
+        async with aiohttp.ClientSession() as session:
+            response = await session.request(method = method, url = self.host + function, headers = headers, data = data)
+            content = await response.json()
