027e897515511bd7fef041958bc48dba773d1153
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-05-10 17:28:51 +0530

    update number_api.py

--- NumberApi/number_api.py
+++ NumberApi/number_api.py
@@ -9,3 +9,10 @@ class BaseHQApi:
         async with aiohttp.ClientSession() as session:
             response = await session.request(method = method, url = self.host + function, headers = headers, data = data)
             content = await response.json()
+            error = content.get("error")
+            if error:
+                raise error
+            return content
+
+    async def get_number(self, service):
+        return await self.fetch("GET", "?act=getnumber&service={}&accessCode={}".format(service, self.access_code))
