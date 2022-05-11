e6c0239c82b20ad667a72652e4068c4f1c6a139f
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-05-11 07:31:42 +0530

    update number.py

--- Cogs/number.py
+++ Cogs/number.py
@@ -46,7 +46,7 @@ class Number(commands.Cog, NumberApi):
         embed.set_thumbnail(url = self.client.user.avatar_url)
         embed.set_footer(text = "Requested by : {}".format(ctx.author), icon_url = ctx.author.avatar_url)
         await ctx.send(embed = embed)
-        self.data["activation_id"] = False
+        self.data[activation_id] = False
         self.data["price"] = price
         embed = discord.Embed(title = "Number Buyer Information !", color = discord.Colour.random())
         embed.description = f"• Username : {ctx.author}\n• User ID : {ctx.author.id}\n• Service : {service.title()}\n• Number : {number}\n• Activation ID : {activation_id}\n• Remaining Balance : ₹{balance}\n"
@@ -62,13 +62,13 @@ class Number(commands.Cog, NumberApi):
         if error: return await ctx.send(ctx.author.mention + "\n```\n" + error + "\n```")
         sms = response.get("sms")
         balance = response.get("balance")
-        check = self.data.get("activation_id")
+        check = self.data.get(activation_id)
         if not check:
             price = self.data.get("price")
             points = db.user.find_one({"user_id": ctx.author.id}).get("points")
             update = {"points": points - price}
             db.user.update_one({"user_id": ctx.author.id}, {"$set": update})
-            self.data["activation_id"] = True
+            self.data[activation_id] = True
         if sms:
             await ctx.send(ctx.author.mention + "\n```\n" + str(sms) + "\n```")
         else:
