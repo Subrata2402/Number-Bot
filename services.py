e934920358027c5ee37bd1ad6a930f0dc7befd6c
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-05-11 05:54:54 +0530

    update services.py

--- services.py
+++ services.py
@@ -1,9 +1,10 @@
 service_list = {
-    "swagbucks": 10,
+    "swagbucks": 9,
     "coindcx": 15,
     "discord": 12,
     "google": 15,
     "flipkart": 10,
     "amazon": 15,
     "mimir": 12,
+    "telegram": 15,
 }
