d52cc69a2d5e50489717b701980ab46459da3c33
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-06-24 20:35:30 +0530

    Delete database directory

--- database/db.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-
-data = MongoClient('mongodb+srv://Subrata3250:subrata3250@cluster0.gqwt8.mongodb.net/NumberStore?retryWrites=true&w=majority')
-db = data.get_database("NumberStore")
-user = db.points
